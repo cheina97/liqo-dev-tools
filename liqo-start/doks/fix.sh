14ef6a045666227f03a38a66e694bc9fed8dfb0e
Francesco Cheinasso <cheinasso.francesco@gmail.com>
2023-02-01 17:03:59 +0100

    Refactoring

--- liqo-start/doks/fix.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-export KUBECONFIG="$HOME/liqo_kubeconfig_do1.yaml"
-kubectl create deployment --image nginx lb-replier -n liqo
-kubectl patch service liqo-gateway --patch-file ./fix.sh -n liqo
